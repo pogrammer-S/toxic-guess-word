b4f36c406d0b23939cd87a8f714e19fb4a3c1f42
pogrammer-S <ilarumancev69@gmail.com>
2025-08-20 12:07:52 +0700

    html исправлаение 2

--- bot/src/backend/back_service.py
+++ bot/src/backend/back_service.py
@@ -11,14 +11,14 @@ def start_game(user_id : int):
         session = return_session(user_id)
         return requests.get(url, headers={'user_id': session}).json()
     except Exception as e:
-        return f"ошибка при выполнении запроса: {e}"
+        return {"answer_game": f"ошибка при выполнении запроса: {e}"}
 
 def answer(user_id : int, message : str):
     try:
         session = return_session(user_id)
         return requests.get(urljoin(url, "return_word/"), headers={'user_id': session}, params={"message": message}).json()
     except Exception as e:
-        return f"ошибка при выполнении запроса: {e}"
+        return {"answer_game": f"ошибка при выполнении запроса: {e}"}
 
 def help(user_id : int):
     try:
