694c672edb6b4f0b52920ef773495d07c4914304
pogrammer-S <ilarumancev69@gmail.com>
2025-08-21 12:21:25 +0700

    Логи в api

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,12 @@
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
 from src.config.config import load_config
 from src.model.model_manager import ModelManager
 from src.model.connect import model
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
-import logging
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
     
 
 config = load_config()
@@ -14,8 +14,6 @@ model_manager=ModelManager()
 from src.game.game_class import Game
 game = Game()
 app = FastAPI()
-    
-templates = Jinja2Templates(directory="src/fastapi")
 
 game_state = {
     "random_word": game.random_word,
