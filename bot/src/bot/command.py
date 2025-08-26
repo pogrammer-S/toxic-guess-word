29c13a78b30743c94b8635c2ec9e08ae6a7d6564
pogrammer-S <ilarumancev69@gmail.com>
2025-08-26 10:16:34 +0700

    HTML форматирование

--- bot/src/bot/command.py
+++ bot/src/bot/command.py
@@ -16,7 +16,7 @@ def command_start(message):
         answer_server = backend_client.start_game()
         logging.info(f"Юзер: {message.from_user.id}, сессия: {answer_server["session_id"]}")
         save_session_id(answer_server["session_id"], message.from_user.id)
-        bot.send_message(message.from_user.id, answer_server["message"])
+        bot.send_message(message.from_user.id, f"<b>{answer_server['message']}</b>", parse_mode="HTML")
     except Exception as e:
         bot.send_message(message.chat.id, f"Ошибка:\n<b>{e}</b>", parse_mode="HTML")
         logging.error(f"Ошибка команда /start:{e}")
@@ -25,15 +25,11 @@ def command_help(message):
     try:
         logging.info("Полученна команда /help")
         answer_server = backend_client.help(return_session_id(message.from_user.id))
-        """
-        if "Помощь" in answer_server["message"]:
-            html_list = ast.literal_eval(answer_server[answer_server.find('['):answer_server["message"].rfind(']') + 1])
-            bot.send_message(message.chat.id, f"<b>Подсказка:</b>\n{html_list}", parse_mode="HTML")
-        else:
-            bot.send_message(message.chat.id, str(answer_server))
-        """
         logging.info(f"{answer_server}")
-        bot.send_message(message.from_user.id, f"{str(get_min_distanse(answer_server))}")
+        if len(answer_server['previous_words']) == 0:
+            bot.send_message(message.from_user.id, f"<b>Для подсказки необходимо ввести любое слово</b>", parse_mode="HTML")
+        else:
+            bot.send_message(message.from_user.id, f"<b>Слово межу вашим и правильным:</b>\n{str(get_min_distanse(answer_server))}", parse_mode="HTML")
     except Exception as e:
         bot.send_message(message.chat.id, f"Ошибка:\n<b>{e}</b>", parse_mode="HTML")
         logging.error(f"Ошибка команда /help:{e}")
--- bot/src/bot/handler.py
+++ bot/src/bot/handler.py
@@ -16,24 +16,18 @@ def handler_message(message):
         if session_id != None:
             answer_server = backend_client.answer(return_session_id(message.from_user.id), message.text)
             logging.info(f"{str(answer_server)}")
+            if answer_server == None:
+                bot.send_message(message.chat.id, f"<b>Ошибка запроса</b>", parse_mode="HTML")
+                return None
         else:
             bot.send_message(message.chat.id, f"<b>Сессия не найдена</b>", parse_mode="HTML")
             return None
-        if answer_server["message"] == "Нет слова":
-            bot.send_message(message.chat.id, f"<b>Слово <i>{message.text}</i> отсутствует в модели.</b>", parse_mode="HTML")
-        elif answer_server["message"] == "Только существительные":
-            bot.send_message(message.chat.id, "<b>Только существительные</b>", parse_mode="HTML")
-        elif "Победа" in answer_server["message"].split():
-            tryer = answer_server["tryers"]
-            helper = ast.literal_eval(answer_server[answer_server.find('['):answer_server["message"].rfind(']') + 1])
-            bot.send_message(message.chat.id, f"<b>Вы победили за {tryer} попыток. Загаданно новое слово</b>\n<i>Подсказка:</i>\n{helper}", parse_mode="HTML")
-        elif answer_server["message"] == "Было":
-            bot.bot.send_message(message.chat.id, "<b>Это слово уже было</b>", parse_mode="HTML")
-        elif "Неверно" in answer_server["message"].split():
-            dist = answer_server["message"].split()[1]
-            bot.send_message(message.chat.id, f"<b>Неправильное слово.</b>\n<i>Расстояние до правильного: {dist}</i>", parse_mode="HTML")
+
+        if answer_server["is_completed"] == True:
+            bot.send_message(message.chat.id, f"Вы победили за {answer_server['attempts_left']} попыток\n<b>Игра завершена. Начните новую игру командой</b> /start", parse_mode="HTML")
         else:
-            bot.send_message(message.chat.id, answer_server["message"], parse_mode="HTML")
+            bot.send_message(message.chat.id, f"<b>{answer_server['message']}</b>", parse_mode="HTML")
+            
     except Exception as e:
         bot.send_message(message.chat.id, f"Ошибка:\n<b>{e}</b>", parse_mode="HTML")
         logging.error(f"Ошибка сообщения:{e}")
